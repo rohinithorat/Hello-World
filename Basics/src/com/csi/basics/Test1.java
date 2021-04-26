ed2ddbcf1a24c9fd49d6b772eab6c6c083107360
user <user@DESKTOP-7066AM5>
2021-04-26 19:09:11 +0530

    First Commit

---
+++ Basics/src/com/csi/basics/Test1.java
@@ -0,0 +1,18 @@
+package com.csi.basics;
+
+public class Test1 {
+	  
+	    public static void main (String args[])   
+	    {  
+	        for(int i=0;true; i++)   
+	        {  
+	            System.out.println("Hello Javatpoint");
+	            if(i==7)
+	            {
+	            	break;
+	            	
+	            }
+	        }  
+	    }  
+	}  
+
